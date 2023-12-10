b9ec4758f6bd1637d4f1dd0ec3a6bd1edbc7888d
Traderxue <falethe@gmail.com>
2023-12-10 22:58:05 +0800

    init

---
+++ src/views/home/home.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="home">
+        <div class="header">
+            <span>菜单</span><span>首页</span><span>语言</span>
+        </div>
+        <div class="banner">
+            banner轮播图
+        </div>
+    </div>
+</template>
+
+<style scoped lang="scss">
+.home{
+    width: auto;
+    height: auto;
+    padding: 15px;
+    .header{
+        width: auto;
+        height: auto;
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+    }
+    .banner{
+        margin: 10px 0px;
+    }
+}
+</style>
