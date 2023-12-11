547517122ee34baf9de7c3cd50a9bbadd685a683
Traderxue <falethe@gmail.com>
2023-12-11 13:20:16 +0800

    init

--- src/views/index.vue
+++ src/views/index.vue
@@ -1,16 +1,16 @@
 <script setup>
 import { ref } from "vue";
-import {showToast} from "vant"
+import { showToast } from "vant";
 
 const active = ref(0);
 const loading = ref(false);
-    const onRefresh = () => {
-      window.location.reload()
-      setTimeout(() => {
-        showToast('刷新成功');
-        loading.value = false;
-      }, 1000);
-    };
+const onRefresh = () => {
+  window.location.reload();
+  setTimeout(() => {
+    showToast("刷新成功");
+    loading.value = false;
+  }, 1000);
+};
 </script>
 
 <template>
@@ -27,10 +27,10 @@ const loading = ref(false);
         <span class="material-symbols-outlined"> monitoring </span
         ><br />行情</van-tabbar-item
       >
-      <van-tabbar-item to="contract"
+      <van-tabbar-item to="/contract"
         ><span class="material-symbols-outlined"> list_alt </span><br />合约
       </van-tabbar-item>
-      <van-tabbar-item to="mine"
+      <van-tabbar-item to="/mine"
         ><span class="material-symbols-outlined"> person </span
         ><br />我的</van-tabbar-item
       >
--- src/views/mine/mine.vue
+++ src/views/mine/mine.vue
@@ -1,3 +1,24 @@
 <template>
-    <div>mine</div>
+  <div class="mine">
+    <div class="header">
+      <span>我的 </span>
+    </div>
+    <div class="banner">
+        <h3>96132 USDT</h3>
+        <div>
+            
+        </div>
+    </div>
+  </div>
 </template>
+
+<style scoped lang="scss">
+    .mine{
+        width: auto;
+        height: auto;
+        padding: 15px;
+        .header{
+            text-align: center;
+        }
+    }
+</style>
